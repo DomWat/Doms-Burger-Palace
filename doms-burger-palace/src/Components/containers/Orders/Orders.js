f1ad072f0ec2c1c4e3acb0feb9e8cd4fb4e5b82a
DomWat <domwat20@gmail.com>
2020-12-15 12:55:13 -0600

    finish redux

--- doms-burger-palace/src/Components/containers/Orders/Orders.js
+++ doms-burger-palace/src/Components/containers/Orders/Orders.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Order from '../../Order/Order'
-import axios from '../../../axios-orders'
+import * as actions from '../../../store/actions/index'
+import Spinner from '../../UI/Spinner/Spinner'
 
 
 class Orders extends Component {
 
-    state = {
-        orders: [],
-        loading: true
-    }
-    
     componentDidMount() {
-        axios.get('/orders.json')
-            .then(res => {
-                const fetchedOrders = []
-                for(let key in res.data) {
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    })
-                }
-                this.setState({loading: false, orders: fetchedOrders})
-            })
-            .catch(err => {
-                this.setState({loading: false})
-            })
+        this.props.onFetchOrders()
     }
 
     render() {
-        return (
+        let orders = <Spinner />
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ) )
+        }
+    return(
             <div>
-                {this.state.orders.map(order => {
-                    return (
-                        <Order 
-                        key = {order.id}
-                        ingredients = {order.ingredients} 
-                        price = {order.price} />
-                    )
-                })}
-            </div>
+                {orders}
+            </div >
         )
     }
 }
 
-export default Orders
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders)
--- doms-burger-palace/src/store/actions/index.js
+++ doms-burger-palace/src/store/actions/index.js
@@ -5,5 +5,6 @@ export {
 } from './burgerBuilder'
 export { 
     purchaseBurger,
-    puchaseInit
+    puchaseInit,
+    fetchOrders
 } from './order'
